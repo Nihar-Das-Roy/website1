9429b795b97c14b14c437318d918da4477548483
Nihar Das Roy <swapandasroy972@gmail.com>
2026-01-22 14:10:58 +0600

    rebuild for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/website1/",
   plugins: [react()],
+  base: "/website1/",   // 🔥 repo name exactly same
 });
 
 
